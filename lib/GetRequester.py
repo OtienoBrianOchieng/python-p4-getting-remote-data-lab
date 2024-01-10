2d170f69a206ff9ebf39a88be61bca830d184f52
root <root@DESKTOP-UEO62DE.localdomain>
2024-01-10 11:54:58 +0300

    Complete get requests lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,23 @@ import requests
 import json
 
 class GetRequester:
+    
 
     def __init__(self, url):
         self.url = url
+        
 
     def get_response_body(self):
-        pass
+        response = request.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        python_list = []
+        list_members = json.loads(self.get_response_body())
+        for list_member in list_members:
+            python_list.append(list_member['name'])
+
+        return python_list 
+        
+        
+        
